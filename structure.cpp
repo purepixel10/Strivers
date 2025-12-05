82c8c444f611a1e391d4f1d02e08bc64a4a12d4e
purepixel10 <vritikareddy10@gmail.com>
2025-12-05 19:27:12 +0530

    Add files via upload

---
+++ structure.cpp
@@ -0,0 +1,42 @@
+/* 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<< "hello world"<<endl;
+    return 0;
+}*/
+
+
+//printing names
+
+
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Hey Ritika"<<endl;
+    cout<<"Hey Suneela"<<endl;
+}*/
+
+//how to take input
+
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    cout<<"The Value of x is:"<<x;
+    return 0;
+}*/
+
+//to print two variables
+
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x,y;
+    cin>>x>>y;
+    cout<<"The Value of x:"<<x << "and y:"<<y;
+    return 0;
+}*/
+
+
