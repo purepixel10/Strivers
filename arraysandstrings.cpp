c2866a1ab02992080c149b2e8110732ffb4361ea
purepixel10 <vritikareddy10@gmail.com>
2025-12-05 19:25:48 +0530

    Add files via upload

---
+++ arraysandstrings.cpp
@@ -0,0 +1,43 @@
+ //ARRAY
+
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[5];
+    cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    arr[3]=16;
+    cout<<arr[3]<<endl;
+    arr[2]+=10;
+    cout<<arr[2]<<endl;
+    return 0;
+}*/
+
+
+//2D ARRRAY
+
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //2D array
+    int arr[3][5];
+    arr[1][3]=78;
+    cout<<arr[1][3];
+    return 0;
+}*/
+
+
+//STRINGS
+
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="Ritika";
+    cout<<s[2];
+    int len=s.size();
+    cout<<s[len-1];
+    return 0;
+}*/
+
+
+//Characters must be in single quotes
+//Strings must be in double quotes
