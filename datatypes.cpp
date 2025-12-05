bf45383fa33756915fad3e5e5f261ce51eab0b9e
purepixel10 <vritikareddy10@gmail.com>
2025-12-05 19:25:58 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,60 @@
+/*
+ DATA TYPES
+ 1) int , long , long long
+ 2) float , double
+ 3) string and getline
+ 4) char
+  */
+ 
+ 
+ #include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //int
+    /*int x=10;
+    cout<<x<<endl;
+    //long
+    long y=15;
+    cout<<y<<endl;
+    //long long
+    long long z=150000000;
+    cout<<z<<endl;*/
+
+    //float,double.        "An integer can be stored in both float and double"
+    
+    //float
+    /*float a=5.6;
+    cout<<a<<endl;
+    //double             
+    double b=5;
+    cout<<b<<endl;*/
+
+
+    //string and getline
+    
+    //string
+    /*string s1;
+    string s2;
+    cin>>s1>>s2;
+    cout<<s1<<" "<<s2;*/
+
+//Instead of declaring 2 variables we can just print the whole line by using getline
+
+    /*string str;
+    getline(cin,str);
+    cout<<str;*/
+
+    //character
+
+    /*char ch;
+    cin>>ch;
+    cout<<ch;*/
+
+      //(or)
+
+    /*char ch=r;
+    cout<<ch;*/
+    return 0;
+}
+
+
