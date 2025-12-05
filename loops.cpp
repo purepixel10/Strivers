3acb133bacbf62dbafefcd684ca6cd23dbecb78f
purepixel10 <vritikareddy10@gmail.com>
2025-12-05 19:26:15 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,44 @@
+//FOR LOOP
+
+//to print a name for 10 times
+
+/*#include<bitset>/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    for(i=1;i<=10;i++){
+        cout<<"Ritika"<<endl;
+    }
+    cout<<i<<endl;
+    return 0;
+}*/
+
+
+
+//WHILE LOOP
+
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i=1;
+    while(i<=5){
+        cout<<"Ritika"<<endl;
+        i=i+1;
+    }
+    return 0;
+}*/
+
+
+//DO WHILE
+
+/*#include <iostream>
+using namespace std;
+int main(){
+    int i=2;
+    do{
+        cout<<"Ritika"<<i<<endl;
+        i+i+1;
+    }while(i<=1);
+    cout<<i<<endl;
+    return 0;
+}*/
